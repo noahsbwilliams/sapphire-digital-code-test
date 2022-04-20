cfa7b6d4eb8c025f0325245fdfafd733dd20f376
Noah Williams <public@noahsbwilliams.com>
2022-04-20 15:14:26 -0800

    Disable the old logic

--- update_quality.rb
+++ update_quality.rb
@@ -2,17 +2,14 @@ require_relative 'award.rb'
 
 def update_quality(awards)
   awards.each do |award|
-
     case award.name
     when 'Blue Distinction Plus'
       award.quality = 80
       # Expiration, degradation logic not required
-
     when 'Blue First'
       if award.quality < 50
         award.quality += 1
       end
-    
     when 'Blue Compare'
       case award.expires_in
       when award.expires_in > 10
@@ -25,68 +22,78 @@ def update_quality(awards)
         award.quality = award.quality - award.quality
         # Logic to decrement twice as fast not required
         # since it's getting zeroed anyway.
-
+      end
     when 'Blue Star'
-
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus' && award.name != 'Blue Star'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-
-    # Logic to decrement the award.expires_in field
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus' && award.name != 'Blue Star'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
-    if award.name == 'Blue Star'
-      # if award.quality > 0
-      if award.quality <= 0
-          award.quality = award.quality - award.quality
-      else
-        if award.expires_in >= 0
-          award.quality -= 2
-        else
-          if award.expires_in < 0
-          award.quality -= 4
-          end
-        end
+      case award.quality
+      when award.quality <= 0
+        award.quality = award.quality - award.quality
+      when award.expires_in >= 0
+        award.quality -= 2
+      when award.expires_in < 0
+        award.quality -= 4
       end
     end
   end
+
+  #   if award.name != 'Blue First' && award.name != 'Blue Compare'
+  #     if award.quality > 0
+  #       if award.name != 'Blue Distinction Plus' && award.name != 'Blue Star'
+  #         award.quality -= 1
+  #       end
+  #     end
+  #   else
+  #     if award.quality < 50
+  #       award.quality += 1
+  #       if award.name == 'Blue Compare'
+  #         if award.expires_in < 11
+  #           if award.quality < 50
+  #             award.quality += 1
+  #           end
+  #         end
+  #         if award.expires_in < 6
+  #           if award.quality < 50
+  #             award.quality += 1
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+
+  #   # Logic to decrement the award.expires_in field
+  #   if award.name != 'Blue Distinction Plus'
+  #     award.expires_in -= 1
+  #   end
+
+  #   if award.expires_in < 0
+  #     if award.name != 'Blue First'
+  #       if award.name != 'Blue Compare'
+  #         if award.quality > 0
+  #           if award.name != 'Blue Distinction Plus' && award.name != 'Blue Star'
+  #             award.quality -= 1
+  #           end
+  #         end
+  #       else
+  #         award.quality = award.quality - award.quality
+  #       end
+  #     else
+  #       if award.quality < 50
+  #         award.quality += 1
+  #       end
+  #     end
+  #   end
+  #   if award.name == 'Blue Star'
+  #     # if award.quality > 0
+  #     if award.quality <= 0
+  #         award.quality = award.quality - award.quality
+  #     else
+  #       if award.expires_in >= 0
+  #         award.quality -= 2
+  #       else
+  #         if award.expires_in < 0
+  #         award.quality -= 4
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 end
